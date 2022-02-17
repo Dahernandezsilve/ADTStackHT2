b2122705662a85dbeac612a2c069f64aa0a9d2bf
Diego Hernández <77697812+Dahernandezsilve@users.noreply.github.com>
2022-02-17 16:51:27 -0600

    Archivos iniciales
    Se añaden los archivos iniciales

---
+++ src/DriverProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DriverProgram {
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in);
+        int seleccion;
+        boolean salir = false;
+        while (!salir){
+            System.out.println();
+
+        }
+    }
+}
